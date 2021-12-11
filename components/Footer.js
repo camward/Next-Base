4dd37dbe52194f638f6ecef569736adbeabe10e6
alex <camward@yandex.ru>
2021-12-11 13:47:46 +0400

    работа с изображениями

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,11 @@
+import Image from "next/image";
 import Heading from "./Heading";
 
 const Footer = () => (
   <footer>
-    <Heading tag="h3" text="@copy; 2021" />
+    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    &emsp;
+    <Heading tag="h3" text="2021" />
   </footer>
 );
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "../styles/Navbar.module.scss";
 
@@ -13,7 +14,9 @@ const Navbar = () => {
 
   return (
     <nav className={styles.nav}>
-      <div className={styles.logo}>webDev</div>
+      <div className={styles.logo}>
+        <Image src="/logo.png" width={60} height={60} alt="Logo" />
+      </div>
       <div className={styles.links}>
         {navigation.map(({ id, title, path }) => (
           <Link key={id} href={path}>
